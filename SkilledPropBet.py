8b2a5fbbddcd676b8cf8c5c719f927a617cc2603
Ma <ma@tempesttech.com>
2022-04-03 14:53:18 -0600

    Adding in skilled bets.

--- SkilledPropBet.py
+++ SkilledPropBet.py
@@ -1,22 +1,6 @@
 from predictions import *
-from sportsipy.ncaab.boxscore import BoxscorePlayer
 
 if __name__ == '__main__':
 
-	# plus_minus = plus_minus()
-
-	# # Score Prediction
-	# team1_revised_scores = (predicted_scores[0] + predicted_scores[1]) / 2
-	# team2_revised_scores = (predicted_scores[2] + predicted_scores[3]) / 2
-	# # Favorite is determined by winning team from score prediction.
-	# if team1_revised_scores > team2_revised_scores:
-	# 	favorite = team1[0]
-	# else:
-	# 	favorite = team2[0]
-
-	# # States the PLUS and MINUS lines in terminal.
-	# print('PLUS AND MINUS LINES:')
-	# if favorite == team1[0]:
-	# 	print(f'{team1[0]:20s}: -{round(plus_minus)}\n{team2[0]:20s}: +{round(plus_minus)}')
-	# else:
-	# 	print(f'{team1[0]:20s}: +{round(plus_minus)}\n{team2[0]:20s}: -{round(plus_minus)}')
+	print("\nKansas's player Ochai Agbaji has an average of 19.8 ppg.")
+	print("Will Ochai Agbaji score more than 20 points in the championship game?\n")
---
+++ SkilledPropBet2.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from sportsipy.ncaab.boxscore import Boxscores
+
+if __name__ == '__main__':
+
+	print("\n University of North Carolina's Armando Bacot has an average of 12.48 rebounds per game. ")
+	print("Will Armando Bacot have more than 13 rebounds in the NCAA Championship game?")
