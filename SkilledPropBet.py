cbd5ee40c0b2529f3da81bb990ec3bc1bef94391
Ma <ma@tempesttech.com>
2022-04-03 14:59:12 -0600

    Adjusting the numbers.

--- SkilledPropBet.py
+++ SkilledPropBet.py
@@ -3,4 +3,4 @@ from predictions import *
 if __name__ == '__main__':
 
 	print("\nKansas's player Ochai Agbaji has an average of 19.8 ppg.")
-	print("Will Ochai Agbaji score more than 20 points in the championship game?\n")
+	print("Will Ochai Agbaji score more than 18 points in the championship game?\n")
