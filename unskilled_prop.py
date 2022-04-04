007e3d629bb5c7f7d98004900bd6ecc40f267aad
Cameron Christofferson <cc147209@umconnect.umt.edu>
2022-04-04 11:46:09 -0600

    Unskilled Props

--- unskilled_prop.py
+++ unskilled_prop.py
@@ -1,3 +1,15 @@
 # Which team wiil score first?
 
 # Which team will get more fouls in the game?
+
+def unskilled1():
+	print('Which team will score first?')
+
+def unskilled2():
+	print('Which team will get more fouls in the game?')
+
+if __name__ == '__main__':
+
+	unskilled1()
+	print()
+	unskilled2()
