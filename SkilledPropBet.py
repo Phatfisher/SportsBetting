bbd4754e7ee5a9ede229901da41281da5d9dbc3e
Ma <ma@tempesttech.com>
2022-04-08 13:25:16 -0600

    Additional changes.

--- SkilledPropBet.py
+++ SkilledPropBet.py
@@ -4,3 +4,4 @@ if __name__ == '__main__':
 
 	print("\nKansas's player Ochai Agbaji has an average of 19.8 ppg.")
 	print("Will Ochai Agbaji score more than 18 points in the championship game?\n")
+	print("Yes: -110 \t No: -110")
--- SkilledPropBet2.py
+++ SkilledPropBet2.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
 
 	print("\n University of North Carolina's Armando Bacot has an average of 12.48 rebounds per game. ")
 	print("Will Armando Bacot have more than 13 rebounds in the NCAA Championship game?")
+	print("Yes: -110 \t No: -110")
